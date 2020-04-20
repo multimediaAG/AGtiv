e8ccdd42b5a529db258bb2081fea20a363bbe310
linux-leo <leo@catsconsultants.com>
2020-04-20 14:53:32 +0200

    score

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScoresComponent } from './components/scores/scores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoresComponent
   ],
   imports: [
     BrowserModule,

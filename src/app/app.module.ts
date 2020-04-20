137f330501e4d8ca25fc75d8e5cad1a9619ad79c
Gordian <gogoh60@gmail.com>
2020-04-20 14:28:44 +0200

    Added rules component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RulesComponent } from './components/rules/rules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,

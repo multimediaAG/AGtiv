9222d14e1fe28022ead847dea51c58257ff8fcc8
Gordian <gogoh60@gmail.com>
2020-04-21 17:44:15 +0200

    Fixed missing FormsModule in app.module

--- AGtive/src/app/app.module.ts
+++ AGtive/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

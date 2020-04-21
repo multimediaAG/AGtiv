f81f8098dcd78bd1fba02407143f542557bacdbb
Hannes Rüger <hannesrueger@gmx.de>
2020-04-21 13:57:44 +0200

    added routes to home component and fallback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RulesComponent } from './components/rules/rules.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
   {
     path: "rules",
     component: RulesComponent,
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/home",
   }
 ];
 

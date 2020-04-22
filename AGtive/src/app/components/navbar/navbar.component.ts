79db1b209fc42d37669478fc574b54d2f5209be2
Hannes Rüger <hannesrueger@gmx.de>
2020-04-22 14:09:54 +0200

    added login, logout and register to navbar

--- AGtive/src/app/components/navbar/navbar.component.ts
+++ AGtive/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    //
   }
 
 }

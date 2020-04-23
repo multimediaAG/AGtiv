5e4365ae2bfc932a635c75b2a12d7ec2a72a9186
Hannes Rüger <hannesrueger@gmx.de>
2020-04-23 14:37:07 +0200

    added validation and logic to signup and signin

--- AGtive/src/app/components/signin/signin.component.ts
+++ AGtive/src/app/components/signin/signin.component.ts
@@ -1,35 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthenticationService} from 'src/app/service/authentication.service'
-import { Router } from '@angular/router';
+import {AuthenticationService} from 'src/app/services/authentication.service'
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
-  
 })
 export class SigninComponent implements OnInit {
 
-  public username: string = "";
-  public password: string = "";
-  public formInvalid: boolean = false;
-  public usernameOrPasswordWrong: boolean = false;
+  public signinForm: FormGroup;
+  public submitted: boolean = false;
+  public loading: boolean = false;
 
-  constructor(private authenticationService: AuthenticationService, private router:Router) { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    public route: ActivatedRoute,
+    public formBuilder: FormBuilder,
+    public alertService: AlertService,
+  ) { }
+
   ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    this.signinForm = this.formBuilder.group({
+      username: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+    });
   }
-  
+
+  public get f() {
+    return this.signinForm.controls;
+  }
+
   public login(): void {
-    if (this.username && this.password) {
-      const loginSuccessful: boolean = this.authenticationService.login(this.username, this.password);
-      if (loginSuccessful) {
-        this.usernameOrPasswordWrong = false;
-        this.router.navigate(["/home"]);
-      } else { this.usernameOrPasswordWrong = true; }
-
-    } else { this.formInvalid = true; }
-
+    this.submitted = true;
+    if (this.signinForm.valid) {
+      this.loading = true;
+      this.authenticationService.login(this.f.username.value, this.f.password.value).subscribe((user) => {
+        this.loading = false;
+        // login successful if there's a jwt token in the response
+        if (user && user.token) {
+          // store user details and jwt token in local storage to
+          // keep user logged in between page refreshes
+          sessionStorage.setItem("jwt_token", user.token);
+          this.authenticationService.currentUser = user;
+          this.authenticationService.isLoggedIn = true;
+          this.router.navigate(["/home"]);
+        }
+      });
+    }
   }
 
 
--- AGtive/src/app/components/signup/signup.component.ts
+++ AGtive/src/app/components/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,35 +10,46 @@ import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms"
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
   public signupForm: FormGroup;
+  public submitted: boolean = false;
+  public loading: boolean = false;
 
   public readonly maxUsernameLength: number = 20;
   public readonly minUsernameLength: number = 2;
-  public readonly minPasswordLength: number = 8;
+  public readonly minPasswordLength: number = 6;
+  public readonly minNameLength: number = 6;
 
-  public usernameInvalid: boolean = false;
-  public usernameAlreadyTaken: boolean = false;
-  public passwordsDifferent: boolean = false;
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
       username: new FormControl("", [Validators.minLength(this.minUsernameLength), Validators.maxLength(this.maxUsernameLength), Validators.required]),
+      name: new FormControl("", [Validators.minLength(this.minNameLength), , Validators.required]),
       password: new FormControl("", [Validators.minLength(this.minPasswordLength), Validators.required]),
       passwordVerify: new FormControl("")
     });
   }
 
+  public get f() {
+    return this.signupForm.controls;
+  }
+
   public signup() {
-    this.usernameInvalid = this.signupForm.controls.username.status !== "VALID";
-    this.usernameAlreadyTaken = false; // TODO
-
-    this.passwordsDifferent = (this.signupForm.controls.password.value != this.signupForm.controls.passwordVerify.value);
-
-    if (!this.usernameInvalid && !this.usernameAlreadyTaken && !this.passwordsDifferent) {
-      // Signup
+    this.submitted = true;
+    if (!this.signupForm.invalid) {
+      this.loading = true;
+      this.httpClient.post(`${environment.apiUrl}users`, {
+        username: this.f.username.value,
+        realName: this.f.name.value,
+        password: this.f.password.value,
+        passwordVerify: this.f.passwordVerify.value,
+        grade: this.f.grade,
+      }).subscribe((data: any) => {
+        if (data && data.status) {
+          this.loading = false;
+          this.router.navigate(["/signin"], {queryParams: {signupSuccessfull: true}})
+        }
+      });
     }
   }
 

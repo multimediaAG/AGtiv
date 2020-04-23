81dc8ede9e00b8a6dcbbb936d5660601cf97b36b
Hannes Rüger <hannesrueger@gmx.de>
2020-04-23 14:31:37 +0200

    added remote service

---
+++ AGtive/src/app/services/remote.service.ts
@@ -0,0 +1,85 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { environment } from "../../environments/environment";
+import { AlertService } from "./alert.service";
+
+@Injectable({
+    providedIn: "root",
+})
+export class RemoteService {
+    public constructor(
+        private http: HttpClient,
+        private alertService: AlertService,
+    ) { }
+
+    public get(path: string, args?: any): Observable<any> {
+        return this.do("get", path, args);
+    }
+    public post(path: string, args?: any): Observable<any> {
+        return this.do("post", path, args);
+    }
+    public delete(path: string, args?: any): Observable<any> {
+        return this.do("delete", path, args);
+    }
+
+    private do(type, path, args) {
+        this.log(`fetching ${path} with data ${JSON.stringify(args)}`);
+        return this.getRequest(type, path, args)
+            .pipe(
+                tap((): void => this.log(
+                    `fetched ${
+                        path
+                    } with data ${
+                        JSON.stringify(args)}`,
+                )),
+                catchError(this.handleError<any>(path, false)),
+            );
+    }
+
+    private handleError<T>(operation = "operation", result?: T): any {
+        return (error: any): Observable<T> => {
+            // eslint-disable-next-line no-console
+            console.error("Error occured in remote.service.ts:", error);
+
+            this.log(`${operation} failed: ${error.message}`);
+            // eslint-disable-next-line no-console
+            console.log(result);
+
+            this.alertService.error(error);
+            console.warn("alerted!");
+
+            return of(result as T);
+        };
+    }
+
+    private getRequest(type: string, path: string, args: any): Observable<any> {
+        let req;
+        if (type == "get") {
+            req = this.http.get<any>(`${environment.apiUrl}${path}`, {
+                ...args,
+            });
+        } else if (type == "post") {
+            req = this.http.post<any>(`${environment.apiUrl}${path}`, {
+                ...args,
+            });
+        } else if (type == "put") {
+            req = this.http.put<any>(`${environment.apiUrl}${path}`, {
+                ...args,
+            });
+        } else if (type == "delete") {
+            req = this.http.delete<any>(`${environment.apiUrl}${path}`, {
+                ...args,
+            });
+        }
+        return req;
+    }
+
+    private log(message: string): void {
+        // eslint-disable-next-line no-console
+        console.log(`RemoteService Log: ${message}`);
+    }
+
+
+}

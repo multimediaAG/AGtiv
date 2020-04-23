b06ac13aa4e75624ab4fa70a2df6ab527c88b4c8
Hannes Rüger <hannesrueger@gmx.de>
2020-04-23 14:33:45 +0200

    registered ngx toastr and interceptors

--- AGtive/src/app/app.module.ts
+++ AGtive/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { HomeComponent } from './components/home/home.component';
 import { ScoresComponent } from './components/scores/scores.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -29,10 +34,16 @@ import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

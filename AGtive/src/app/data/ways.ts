343ab13c76a2102c80f521a65f4132e9b49b208f
Hannes Rüger <hannesrueger@gmx.de>
2020-04-24 13:15:00 +0200

    added first ways

---
+++ AGtive/src/app/data/ways.ts
@@ -0,0 +1,19 @@
+const ways = [
+    {
+        city: "Kempten",
+        country: "DE",
+        distance: "0"
+    },
+    {
+        city: "Marseille",
+        country: "IT",
+        distance: "964"
+    },
+];
+
+const countries = [
+    {
+        code: "DE",
+        name: "Deutschland"
+    }
+];

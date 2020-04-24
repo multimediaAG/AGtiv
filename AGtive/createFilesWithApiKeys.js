aee5bcab1f4cc6a6edfd54724d81ef059dcd5986
Hannes Rüger <hannesrueger@gmx.de>
2020-04-24 14:15:38 +0200

    fixed build error

--- AGtive/createFilesWithApiKeys.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-let content = 'export const environment = {production: false, apiUrl: ""};';
-fs.writeFileSync(path.join(__dirname, "./src/environments/environment.ts"), content);
-content = 'export const environment = {production: true, apiUrl: ""};';
-fs.writeFileSync(path.join(__dirname, "./src/environments/environment.prod.ts"), content);

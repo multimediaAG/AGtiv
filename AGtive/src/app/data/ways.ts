750107cedaaefbc90c3c18ec7e9e586839922493
Hannes Rüger <hannesrueger@gmx.de>
2020-04-25 12:11:32 +0200

    refractored and finalized data

--- AGtive/src/app/data/ways.ts
+++ AGtive/src/app/data/ways.ts
@@ -1,389 +1,407 @@
-const ways = [
-    {
-        city: "Kempten",
-        country: "DE",
-        distance: "0"
-    },
-    {
-        city: "Marseille",
-        country: "IT",
-        distance: "964"
-    },
-    {
-        city: "Barcelona",
-        country: "ES",
-        distance: "508"
-    },
-    {
-        city: "Gibraltar",
-        country: "GI",
-        distance: "1108"
-    },
-    {
-        city: "Marrakesch",
-        country: "MA",
-        distance: "653"
-    },
-    {
-        city: "Ad Dakhla",
-        country: "EH",
-        distance: "1406"
-    },
-    {
-        city: "Dakar",
-        country: "SN",
-        distance: "1367"
-    },
-    {
-        city: "Conakry",
-        country: "GN",
-        distance: "1199"
-    },
-    {
-        city: "Makeni",
-        country: "SL",
-        distance: "271"
-    },
-    {
-        city: "Nzerekore",
-        country: "GN",
-        distance: "516"
-    },
-    {
-        city: "Yamoussoukro",
-        country: "CI",
-        distance: "519"
-    },
-    {
-        city: "Kumasi",
-        country: "GH",
-        distance: "516"
-    },
-    {
-        city: "Proto-Novo",
-        country: "BJ",
-        distance: "648"
-    },
-    {
-        city: "Onitsha",
-        country: "NG",
-        distance: "543"
-    },
-    {
-        city: "Douala",
-        country: "CM",
-        distance: "666"
-    },
-    {
-        city: "Oyem",
-        country: "GA",
-        distance: "546"
-    },
-    {
-        city: "Mandilou",
-        country: "GA",
-        distance: "602"
-    },
-    {
-        city: "Dolisie",
-        country: "CG",
-        distance: "445"
-    },
-    {
-        city: "Kinshasa",
-        country: "CD",
-        distance: "390"
-    },
-    {
-        city: "Kinshasa",
-        country: "CD",
-        distance: "390"
-    },
-    {
-        city: "Luanda",
-        country: "AO",
-        distance: "818"
-    },
-    {
-        city: "Lubango",
-        country: "AO",
-        distance: "898"
-    },
-    {
-        city: "Windhuk",
-        country: "NA",
-        distance: "1158"
-    },
-    {
-        city: "Kapstadt",
-        country: "ZA",
-        distance: "1479"
-    },
-    {
-        city: "Port Elizabeth",
-        country: "ZA",
-        distance: "749"
-    },
-    {
-        city: "Durban",
-        country: "ZA",
-        distance: "910"
-    },
-    {
-        city: "Maputo",
-        country: "MZ",
-        distance: "542"
-    },
-    {
-        city: "Harare",
-        country: "ZW",
-        distance: "1163"
-    },
-    {
-        city: "Lilongwe",
-        country: "MW",
-        distance: "817"
-    },
-    {
-        city: "Dodome",
-        country: "TZ",
-        distance: "1312"
-    },
-    {
-        city: "Nairobi",
-        country: "KE",
-        distance: "689"
-    },
-    {
-        city: "Awassa",
-        country: "ET",
-        distance: "1273"
-    },
-    {
-        city: "Dschibouti",
-        country: "DJ",
-        distance: "998"
-    },
-    {
-        city: "Salala",
-        country: "OM",
-        distance: "1549"
-    },
-    {
-        city: "Dubai",
-        country: "AE",
-        distance: "1281"
-    },
-    {
-        city: "Tschahbahar",
-        country: "IR",
-        distance: "667"
-    },
-    {
-        city: "Karatschi",
-        country: "PK",
-        distance: "980"
-    },
-    {
-        city: "Ahmedabad",
-        country: "IN",
-        distance: "851"
-    },
-    {
-        city: "Mumbai",
-        country: "IN",
-        distance: "525"
-    },
-    {
-        city: "Madurai",
-        country: "IN",
-        distance: "1427"
-    },
-    {
-        city: "Kakinada",
-        country: "IN",
-        distance: "1123"
-    },
-    {
-        city: "Kalkutta",
-        country: "IN",
-        distance: "1025"
-    },
-    {
-        city: "Mandalay",
-        country: "MM",
-        distance: "1399"
-    },
-    {
-        city: "Rangun",
-        country: "MM",
-        distance: "628"
-    },
-    {
-        city: "Bangkok",
-        country: "TH",
-        distance: "837"
-    },
-    {
-        city: "Phnom Penh",
-        country: "KD",
-        distance: "719"
-    },
-    {
-        city: "Pleiku",
-        country: "VN",
-        distance: "627"
-    },
-    {
-        city: "Hanoi",
-        country: "VN",
-        distance: "1102"
-    },
-];
+// tslint:disable-next-line
+export module data {
+    export interface City {
+        name: string;
+        country: string;
+        distance: number;
+    }
 
-const countries = [
-    {
-        code: "DE",
-        name: "Deutschland"
-    },
-    {
-        code: "IT",
-        name: "Italien"
-    },
-    {
-        code: "ES",
-        name: "Spanien"
-    },
-    {
-        code: "GI",
-        name: "Gibraltar"
-    },
-    {
-        code: "MA",
-        name: "Marokko"
-    },
-    {
-        code: "EH",
-        name: "Westsahara"
-    },
-    {
-        code: "SN",
-        name: "Senegal"
-    },
-    {
-        code: "GN",
-        name: "Guinea"
-    },
-    {
-        code: "SL",
-        name: "Sierra Leone"
-    },
-    {
-        code: "CI",
-        name: "Elfenbeinküste"
-    },
-    {
-        code: "GH",
-        name: "Ghana"
-    },
-    {
-        code: "BJ",
-        name: "Benin"
-    },
-    {
-        code: "NG",
-        name: "Nigeria"
-    },
-    {
-        code: "CM",
-        name: "Kamerun"
-    },
-    {
-        code: "GA",
-        name: "Gabun"
-    },
-    {
-        code: "CG",
-        name: "Kongo"
-    },
-    {
-        code: "CD",
-        name: "D. R. Kongo"
-    },
-    {
-        code: "VN",
-        name: "Vietnam"
-    },
-    {
-        code: "KH",
-        name: "Kambodscha"
-    },
-    {
-        code: "TH",
-        name: "Thailand"
-    },
-    {
-        code: "MM",
-        name: "Myanmar"
-    },
-    {
-        code: "IN",
-        name: "Indien"
-    },
-    {
-        code: "PK",
-        name: "Pakistan"
-    },
-    {
-        code: "IR",
-        name: "Iran"
-    },
-    {
-        code: "AE",
-        name: "Ver. arab. Emirate"
-    },
-    {
-        code: "OM",
-        name: "Oman"
-    },
-    {
-        code: "DJ",
-        name: "Dschibouti"
-    },
-    {
-        code: "ET",
-        name: "Äthiopien"
-    },
-    {
-        code: "AO",
-        name: "Angola"
-    },
-    {
-        code: "NA",
-        name: "Namibia"
-    },
-    {
-        code: "ZA",
-        name: "Südafrika"
-    },
-    {
-        code: "MZ",
-        name: "Mosambik"
-    },
-    {
-        code: "ZW",
-        name: "Simbabwe"
-    },
-    {
-        code: "MW",
-        name: "Malawi"
-    },
-    {
-        code: "TZ",
-        name: "Tansania"
-    },
-    {
-        code: "KE",
-        name: "Kenia"
-    },
-];
+    export interface Country {
+        code: string;
+        name: string;
+    }
+
+    export const cities: City[] = [
+        {
+            name: "Kempten",
+            country: "de",
+            distance: 0
+        },
+        {
+            name: "Marseille",
+            country: "fr",
+            distance: 964
+        },
+        {
+            name: "Barcelona",
+            country: "es",
+            distance: 508
+        },
+        {
+            name: "Gibraltar",
+            country: "gi",
+            distance: 1108
+        },
+        {
+            name: "Marrakesch",
+            country: "ma",
+            distance: 653
+        },
+        {
+            name: "Ad Dakhla",
+            country: "eh",
+            distance: 1406
+        },
+        {
+            name: "Dakar",
+            country: "sn",
+            distance: 1367
+        },
+        {
+            name: "Conakry",
+            country: "gn",
+            distance: 1199
+        },
+        {
+            name: "Makeni",
+            country: "sl",
+            distance: 271
+        },
+        {
+            name: "Nzerekore",
+            country: "gn",
+            distance: 516
+        },
+        {
+            name: "Yamoussoukro",
+            country: "ci",
+            distance: 519
+        },
+        {
+            name: "Kumasi",
+            country: "gh",
+            distance: 516
+        },
+        {
+            name: "Proto-Novo",
+            country: "bj",
+            distance: 648
+        },
+        {
+            name: "Onitsha",
+            country: "ng",
+            distance: 543
+        },
+        {
+            name: "Douala",
+            country: "cm",
+            distance: 666
+        },
+        {
+            name: "Oyem",
+            country: "ga",
+            distance: 546
+        },
+        {
+            name: "Mandilou",
+            country: "ga",
+            distance: 602
+        },
+        {
+            name: "Dolisie",
+            country: "cg",
+            distance: 445
+        },
+        {
+            name: "Kinshasa",
+            country: "cd",
+            distance: 390
+        },
+        {
+            name: "Kinshasa",
+            country: "cd",
+            distance: 390
+        },
+        {
+            name: "Luanda",
+            country: "ao",
+            distance: 818
+        },
+        {
+            name: "Lubango",
+            country: "ao",
+            distance: 898
+        },
+        {
+            name: "Windhuk",
+            country: "na",
+            distance: 1158
+        },
+        {
+            name: "Kapstadt",
+            country: "za",
+            distance: 1479
+        },
+        {
+            name: "Port Elizabeth",
+            country: "za",
+            distance: 749
+        },
+        {
+            name: "Durban",
+            country: "za",
+            distance: 910
+        },
+        {
+            name: "Maputo",
+            country: "mz",
+            distance: 542
+        },
+        {
+            name: "Harare",
+            country: "zw",
+            distance: 1163
+        },
+        {
+            name: "Lilongwe",
+            country: "mw",
+            distance: 817
+        },
+        {
+            name: "Dodome",
+            country: "tz",
+            distance: 1312
+        },
+        {
+            name: "Nairobi",
+            country: "ke",
+            distance: 689
+        },
+        {
+            name: "Awassa",
+            country: "et",
+            distance: 1273
+        },
+        {
+            name: "Dschibouti",
+            country: "dj",
+            distance: 998
+        },
+        {
+            name: "Salala",
+            country: "om",
+            distance: 1549
+        },
+        {
+            name: "Dubai",
+            country: "ae",
+            distance: 1281
+        },
+        {
+            name: "Tschahbahar",
+            country: "ir",
+            distance: 667
+        },
+        {
+            name: "Karatschi",
+            country: "pk",
+            distance: 980
+        },
+        {
+            name: "Ahmedabad",
+            country: "in",
+            distance: 851
+        },
+        {
+            name: "Mumbai",
+            country: "in",
+            distance: 525
+        },
+        {
+            name: "Madurai",
+            country: "in",
+            distance: 1427
+        },
+        {
+            name: "Kakinada",
+            country: "in",
+            distance: 1123
+        },
+        {
+            name: "Kalkutta",
+            country: "in",
+            distance: 1025
+        },
+        {
+            name: "Mandalay",
+            country: "mm",
+            distance: 1399
+        },
+        {
+            name: "Rangun",
+            country: "mm",
+            distance: 628
+        },
+        {
+            name: "Bangkok",
+            country: "th",
+            distance: 837
+        },
+        {
+            name: "Phnom Penh",
+            country: "kd",
+            distance: 719
+        },
+        {
+            name: "Pleiku",
+            country: "vn",
+            distance: 627
+        },
+        {
+            name: "Hanoi",
+            country: "vn",
+            distance: 1102
+        },
+    ];
+
+    export const countries: Country[] = [
+        {
+            code: "de",
+            name: "Deutschland"
+        },
+        {
+            code: "fr",
+            name: "Frankreich"
+        },
+        {
+            code: "it",
+            name: "Italien"
+        },
+        {
+            code: "es",
+            name: "Spanien"
+        },
+        {
+            code: "gi",
+            name: "Gibraltar"
+        },
+        {
+            code: "ma",
+            name: "Marokko"
+        },
+        {
+            code: "eh",
+            name: "Westsahara"
+        },
+        {
+            code: "sn",
+            name: "Senegal"
+        },
+        {
+            code: "gn",
+            name: "Guinea"
+        },
+        {
+            code: "sl",
+            name: "Sierra Leone"
+        },
+        {
+            code: "ci",
+            name: "Elfenbeinküste"
+        },
+        {
+            code: "gh",
+            name: "Ghana"
+        },
+        {
+            code: "bj",
+            name: "Benin"
+        },
+        {
+            code: "ng",
+            name: "Nigeria"
+        },
+        {
+            code: "cm",
+            name: "Kamerun"
+        },
+        {
+            code: "ga",
+            name: "Gabun"
+        },
+        {
+            code: "cg",
+            name: "Kongo"
+        },
+        {
+            code: "cd",
+            name: "D. R. Kongo"
+        },
+        {
+            code: "vn",
+            name: "Vietnam"
+        },
+        {
+            code: "kh",
+            name: "Kambodscha"
+        },
+        {
+            code: "th",
+            name: "Thailand"
+        },
+        {
+            code: "mm",
+            name: "Myanmar"
+        },
+        {
+            code: "in",
+            name: "Indien"
+        },
+        {
+            code: "pk",
+            name: "Pakistan"
+        },
+        {
+            code: "ir",
+            name: "Iran"
+        },
+        {
+            code: "ae",
+            name: "Ver. arab. Emirate"
+        },
+        {
+            code: "om",
+            name: "Oman"
+        },
+        {
+            code: "dj",
+            name: "Dschibouti"
+        },
+        {
+            code: "et",
+            name: "Äthiopien"
+        },
+        {
+            code: "ao",
+            name: "Angola"
+        },
+        {
+            code: "na",
+            name: "Namibia"
+        },
+        {
+            code: "za",
+            name: "Südafrika"
+        },
+        {
+            code: "mz",
+            name: "Mosambik"
+        },
+        {
+            code: "zw",
+            name: "Simbabwe"
+        },
+        {
+            code: "mw",
+            name: "Malawi"
+        },
+        {
+            code: "tz",
+            name: "Tansania"
+        },
+        {
+            code: "ke",
+            name: "Kenia"
+        },
+    ];
+}

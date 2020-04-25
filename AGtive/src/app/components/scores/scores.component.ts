93fc99ac24463a8bd682c05ac518593e55c4ac54
Hannes Rüger <hannesrueger@gmx.de>
2020-04-25 13:59:11 +0200

    added scores list

--- AGtive/src/app/components/scores/scores.component.ts
+++ AGtive/src/app/components/scores/scores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RemoteService } from 'src/app/services/remote.service';
 
 @Component({
   selector: 'app-scores',
@@ -6,5 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scores.component.scss']
 })
 export class ScoresComponent {
+  public users: any[] = [];
+  public maxDistance: number = 1000000000;
+  constructor(private remoteService: RemoteService) { }
 
+  public ngOnInit() {
+    this.remoteService.get("users").subscribe((data) => {
+      if (data) {
+        this.users = data;
+        let place = 1;
+        let lastUser;
+        for (const user of this.users) {
+          if (lastUser && lastUser.distance && user.distance < lastUser.distance) {
+            place++;
+          }
+          user.place = place;
+          lastUser = user;
+        }
+        setTimeout(() => {
+          this.maxDistance = this.users.reduce((p, c) => p.distance > c.distance ? p : c).distance;
+        }, 20);
+      }
+    });
+  }
 }

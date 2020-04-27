2e9da6e2f6dc9550cc44d82efd37929acda22ee9
Hannes Rüger <hannesrueger@gmx.de>
2020-04-27 13:40:37 +0200

    better home screen

--- AGtiv/src/app/app.component.ts
+++ AGtiv/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AGtiv';
+  constructor(private router: Router) {}
+  public isHomeScreen() {
+    return this.router.url.indexOf("home") != -1;
+  }
 }

9a26ddd62502a776952d165aa8b7dfe73c28e17d
Hannes Rüger <hannesrueger@gmx.de>
2020-04-28 07:25:23 +0200

    updated rules

--- AGtiv/src/app/components/rules/rules.component.ts
+++ AGtiv/src/app/components/rules/rules.component.ts
@@ -9,25 +9,28 @@ export class RulesComponent {
 
   rules: any[] = [
     {
-      title: "Nutzername",
-      content: "Verwende keinen anstößigen, beleidigenden oder anderweitig unangebrachte Nutzernamen.",
+      title: "Benutzername",
+      content: "Verwende keinen anstößigen, beleidigenden oder anderweitig unangebrachten Benutzernamen. Du wirst ansonsten gesperrt!",
       icon: "fa-quote-right",
     },
     {
       title: "Passwort",
-      content: "Verwende ein sicheres Passwort und gib es niemals weiter!",
+      content: "Verwende ein sicheres Passwort und gib es niemals weiter! Das Passwort muss mindestens 6 zeichen lang sein.\
+      Merke Dir Deine Zugangsdaten (Benutzername und Passwort) genau.",
       icon: "fa-lock",
     },
     {
       title: "Strecken eingeben",
-      content: "AGtiv basiert auf der Ehrlichkeit und Kameradschaftlichkeit jeder und jedes Einzelnen. Trage nur die Kilometer ein, die auch wirklich geradelt, gelaufen oder skigerollert bist.",
+      content: "AGtiv basiert auf der Ehrlichkeit und Kameradschaftlichkeit jeder und jedes Einzelnen.\
+      Trage nur die Kilometer ein, die auch wirklich geradelt, gelaufen oder skigerollert bist.",
       icon: "fa-file-signature",
     },
     {
       title: "Was zählt?",
-      content: "Es zählen Strecken, die mit dem Fahrrad oder dem Skiroller zurückgelegt worden sind oder Strecken,\
-      die gelaufen wurden. Alle Strecken werden in Fahrradkilometer umgerechnet, um sie vergleichen zu können.\
-      Gemäß des größeren Energieaufwands werden Laufstrecken mit dem Faktor 4 und Skirollerstrecken mit dem Faktor 2.5 automatisch multipliziert.",
+      content: "Es zählen Strecken, die mit dem Fahrrad oder dem Skiroller zurückgelegt oder die gelaufen wurden.\
+      Alle eingetragenen Strecken werden in \"Fahrrad-Kilometer\" umgerechnet, um sie vergleichen zu können.\
+      Gemäß des größeren Energieaufwands werden Laufstrecken mit dem Faktor 4 und Skirollerstrecken mit dem Faktor 2.5 automatisch multipliziert.\
+      Die sich daraus ergebenden Streckenlängen werden mathematisch auf ganze Kilometer gerundet.",
       icon: "fa-question",
     }
   ];

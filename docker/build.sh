3cdf834c965ce2c07f67b3cdcc18b3b4120df1ff
Hannes Rüger <hannesrueger@gmx.de>
2021-03-08 11:37:55 +0100

    fixed typo

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
-cd ../AGTiv
+cd ../AGtiv
 npm install
 npm run build -- --outputPath=../docker/frontend
 cd ../api
@@ -7,6 +7,6 @@ npx @zeit/ncc build src/index.ts -o ../docker/backend
 cd ..
 cp ./container-env.json ./docker/container-env.json
 cd ./docker
-PACKAGE_VERSION=$(cat ../AGTiv/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+PACKAGE_VERSION=$(cat ../AGtiv/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t hrueger/agtiv:v$PACKAGE_VERSION .
 rm -r frontend backend

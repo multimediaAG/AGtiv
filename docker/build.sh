84ad39cc02009e4771bd080491fcf617b90c4c5b
Hannes Rüger <hannesrueger@gmx.de>
2021-03-21 13:32:55 +0100

    fixed paths

--- docker/build.sh
+++ docker/build.sh
@@ -2,8 +2,7 @@ cd ../AGtiv
 npm install
 npm run build -- --outputPath=../docker/frontend
 cd ../
-cp  ./api/* ../docker/backend
-cd ..
+cp  ./api/* ./docker/backend
 cp ./container-env.json ./docker/container-env.json
 cd ./docker
 PACKAGE_VERSION=$(cat ../AGtiv/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')

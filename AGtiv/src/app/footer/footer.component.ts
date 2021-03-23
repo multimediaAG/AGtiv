b1aa866542b741544aa83fb3f539185663961074
Hannes Rüger <hannesrueger@gmx.de>
2021-03-23 08:32:04 +0100

    fixed year

--- AGtiv/src/app/footer/footer.component.ts
+++ AGtiv/src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-footer",
@@ -6,5 +6,5 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent {
-
+    public year = new Date().getFullYear();
 }
